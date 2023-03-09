c857311e157dae484270de6137c2daeaa1654987
sami-bendrif <126948183+sami-bendrif@users.noreply.github.com>
2023-03-09 09:46:43 +0100

    Delete panier.js

--- Build-and-Deploy-Ecommerce-Website-main/panier.js
+++
@@ -1,7 +0,0 @@
-// function  shop() {
-//    var a = document.getElementById('t').value;
-//    var q = document.getElementById('q').value;
-//    document.getElementById('p').innerHTML = a;
-//    document.getElementById('qu').innerHTML = q;
-//    window.location.href = 'ta_sh.html?'
-// }
